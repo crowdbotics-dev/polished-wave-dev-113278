996c8db3e39e127182c1d78cbf8ba53fe1e61667
Crowdbotics <engineering@crowdbotics.com>
2023-08-31 23:10:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-wave-dev-113278",
+  url: "https://polished-wave-dev-113278.botics.co",
+  api: "https://polished-wave-dev-113278.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
